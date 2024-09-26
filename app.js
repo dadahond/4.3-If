ae4825b01852aff73ac53996c3d38de102be8523
dadahon <dadahon@gmail.com>
2024-09-26 22:33:00 +0500

    done

---
+++ app.js
@@ -0,0 +1,177 @@
+// If 1
+// let n = prompt("n sonini kiriting");
+// if (n > 0) {
+//   console.log(Number(n) + 1);
+// } else {
+//   console.log(n);
+// }
+// If 2
+// let n = prompt("n sonini kiriting");
+// if (n > 0) {
+//   console.log(Number(n) + 1);
+// } else {
+//   console.log(Number(n) - 2);
+// }
+// If 3
+// let n = prompt("n sonini kiriting");
+// if (n > 0) {
+//   console.log(Number(n) + 1);
+// } else if (n < 0) {
+//   console.log(Number(n) - 2);
+// } else {
+//   console.log(10);
+// }
+// If 4
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// let c = prompt("c sonini kiriting");
+// let n = 0;
+
+// if (a > 0) {
+//   n = n + 1;
+// }
+// if (b > 0) {
+//   n = n + 1;
+// }
+// if (c > 0) {
+//   n = n + 1;
+// }
+// console.log(n);
+// If 5
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// let c = prompt("c sonini kiriting");
+// let musbat = 0;
+// let manfiy = 0;
+
+// if (a > 0) {
+//   musbat = musbat + 1;
+// } else {
+//   manfiy = manfiy + 1;
+// }
+// if (b > 0) {
+//   musbat = musbat + 1;
+// } else {
+//   manfiy = manfiy + 1;
+// }
+// if (c > 0) {
+//   musbat = musbat + 1;
+// } else {
+//   manfiy = manfiy + 1;
+// }
+
+// console.log(`Musbatlar: ${musbat} ta, Manfiylar: ${manfiy} ta`);
+// If 6
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// if (a > b) {
+//   console.log(`Kattasi ${Number(a)}`);
+// } else {
+//   console.log(`Kattasi ${Number(b)}`);
+// }
+// If 7
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// if (a > b) {
+//   console.log(`Kichik sonni t/r 2`);
+// } else {
+//   console.log(`Kichik sonni t/r 1`);
+// }
+// If 8
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// if (a > b) {
+//   console.log(`Kattasi ${Number(a)}`);
+//   console.log(`Kichigi ${Number(b)}`);
+// } else {
+//   console.log(`Kattasi ${Number(b)}`);
+//   console.log(`Kichigi ${Number(a)}`);
+// }
+// If 9
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// if (a > b) {
+//   console.log(`b= ${Number(a)}`);
+//   console.log(`a= ${Number(b)}`);
+// } else {
+//   console.log(`b= ${Number(b)}`);
+//   console.log(`a= ${Number(a)}`);
+// }
+// If 10
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// if (a == b) {
+//   console.log(`a= 0`);
+//   console.log(`b= 0`);
+// } else {
+//   console.log(Number(a) + Number(b));
+//   console.log(Number(b) + Number(a));
+// }
+// If 11
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// if (a == b) {
+//   console.log(`a= 0`);
+//   console.log(`b= 0`);
+// } else if (a > b) {
+//   console.log(`a=${a}`);
+//   console.log(`b=${a}`);
+// } else {
+//   console.log(`a=${b}`);
+//   console.log(`b=${b}`);
+// }
+// If 12
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// let c = prompt("c sonini kiriting");
+// if (a > b && b > c) {
+//   console.log(Number(c));
+// } else if (a > c && b > c) {
+//   console.log(Number(c));
+// } else if (c > b && b > a) {
+//   console.log(Number(a));
+// }
+// If 13
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// let c = prompt("c sonini kiriting");
+// if (a > b && b > c) {
+//   console.log(Number(b));
+// } else if (a < b && b < c) {
+//   console.log(Number(b));
+// } else if (b > a && a > c) {
+//   console.log(Number(a));
+// } else if (c > a && a > b) {
+//   console.log(Number(a));
+// }
+// If 14
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// let c = prompt("c sonini kiriting");
+// if (a > b && b > c) {
+//   console.log(Number(c));
+//   console.log(Number(a));
+// } else if (a < b && b < c) {
+//   console.log(Number(a));
+//   console.log(Number(c));
+// } else if (b > a && a > c) {
+//   console.log(Number(c));
+//   console.log(Number(b));
+// }
+// If 15
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// let c = prompt("c sonini kiriting");
+// if (a > b && b > c) {
+//   console.log(Number(a));
+//   console.log(Number(b));
+// } else if (a < b && b < c) {
+//   console.log(Number(c));
+//   console.log(Number(b));
+// } else if (b > a && a > c) {
+//   console.log(Number(b));
+//   console.log(Number(a));
+// } else if (c > a && a > b) {
+//   console.log(Number(c));
+//   console.log(Number(a));
+// }
